82b905f0c0b672bc2aaa54e4d9654641c770dda0
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-06 14:06:31 +0200

    ajout de la fonction d'affichage des citation stockées

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,6 +2,7 @@ let form = document.querySelector("#quote-form");
 let quoteTextInput = document.querySelector("#quote-text");
 let authorInput = document.querySelector("#quote-author");
 let quoteTypeInput = document.querySelector("#quote-type");
+let quoteList = document.querySelector("#quote-list");
 
 
 if (!localStorage.getItem("collection-citations")) {
@@ -18,6 +19,8 @@ form.addEventListener("submit", (e) => {
   // empêche le rafraichissement automatique de la page
   e.preventDefault();
 
+    quoteList.innerHTML = "";
+    // récupère les valeurs des inputs
     let quoteText = quoteTextInput.value;
     let quoteAuthor = authorInput.value;
     let quoteType = quoteTypeInput.value
@@ -28,6 +31,9 @@ form.addEventListener("submit", (e) => {
   
 });
 
+/**
+ * crée un objet pour sauvegarder la citation, son auteur et son type, puis renvois cet objet
+ */
 function createThisQuote (text, author, type){
     let myQuote = {
         text : text,
@@ -36,6 +42,11 @@ function createThisQuote (text, author, type){
     }
     return myQuote;
 }
+
+/**
+ * Récupère le tableau encodé (crée au début du script sous condition qu'il n'éxiste pas déjà), et le décode pour y ajouter l'objet
+ * citation nouvellement crée. Appel la fonction displayQuote pour afficher les cartes au même moment 
+ */
 function pushQuoteToCollection (newQuote){
     let collectionEncoded = localStorage.getItem("collection-citations");
 
@@ -43,7 +54,31 @@ function pushQuoteToCollection (newQuote){
 
     collectionDecoded.push(newQuote);
 
-    let collectionReEncoded = JSON.stringify(collectionDecoded);
+    displayQuote (collectionDecoded);
 
+    let collectionReEncoded = JSON.stringify(collectionDecoded);
     localStorage.setItem("collection-citations", collectionReEncoded);
+}
+
+// Affiche les cartes stockées dans la collection sauveagrdé dans le localStorage
+function displayQuote(collection){
+    for (let i = 0 ; i < collection.length ; i++){
+
+        let quoteCard = document.createElement("div");
+        quoteCard.classList.add("card");
+
+        let paraTextQuote = document.createElement("p");
+        paraTextQuote.innerHTML = collection[i].text;
+
+        let paraAuthorQuote = document.createElement("p");
+        paraAuthorQuote.innerHTML = `Auteur : ${collection[i].author}`;
+
+        let paraTypeQuote = document.createElement("p");
+        paraTypeQuote.innerHTML = `Catégorie : ${collection[i].type}`;
+
+        quoteCard.insertAdjacentElement("beforeend", paraTextQuote);
+        quoteCard.insertAdjacentElement("beforeend", paraAuthorQuote);
+        quoteCard.insertAdjacentElement("beforeend",paraTypeQuote);
+        quoteList.insertAdjacentElement("beforeend",quoteCard);
+    }
 }
